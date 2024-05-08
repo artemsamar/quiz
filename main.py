d2d0b33e20fbe3a96a4a14c8b21ce526583924ef
artemsamarin110 <33901766-artemsamarin110@users.noreply.replit.com>
2024-05-08 10:35:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+result = 0
+print("Где живёт Криштиану Рональдо?")
+answer = input()
+if answer == "Партугалия":
+    result += 1
+print("Как зовут сына Рональдо?")
+answer = input()
+if answer == "Рональдо":
+    result += 1
+print("Когда родился Криштиано Рональдо?")
+answer = input()
+if answer == "1985 год":
+    result += 1
+print("Сколько детей у Криштиану?")
+answer = input()
+if answer == "четверо":
+    result += 1
+print("Сколько золотых мячей у Рональдо?")
+answer = input()
+if answer == "пять":
+    result += 1
+print(result)
